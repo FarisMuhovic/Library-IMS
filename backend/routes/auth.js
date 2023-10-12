483e6ef07405e9acabcb86d67d36a355aa1d1bb0
FarisMuhovic <faris.fare123456@gmail.com>
2023-10-12 23:03:20 +0200

    Backend Sessions & Redis/SQL

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,19 +5,25 @@ const {comparePasswords} = require("../utils/passwordHasher");
 module.exports = (connection, mysql) => {
   router.post("/login", (req, res) => {
     const {email, password, rememberUser} = req.body;
-   
+
     connection.query(
-      "SELECT email,password FROM staff WHERE email =" + mysql.escape(email),
+      "select email,password,id from staff WHERE email =" + mysql.escape(email), // change later to add lots of info about that manager,
       function (err, results, fields) {
-        if (
-          results[0].email == email &&
-          comparePasswords(password, results[0].password)
-        ) {
-          console.log("logged in");
-          res.sendStatus(200);
-          // setup sessions.
+        if (results.length > 0) {
+          if (comparePasswords(password, results[0].password)) {
+            console.log("logged in");
+            req.session.user = {
+              email: email,
+              id: results[0].id,
+              // here goes other info
+            };
+            console.log(req.session);
+            res.status(200).json({message: "User logged in"});
+          } else {
+            res.status(400).json({message: "Wrong password"});
+          }
         } else {
-          console.log("wrong password");
+          console.log("invalid info");
         }
       }
     );
--- backend/server.js
+++ backend/server.js
@@ -2,44 +2,58 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const app = express();
+// .env secret variables
+require("dotenv").config();
 
-const PORT = 5000; // .env ??
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`The Express server is running on port ${PORT}`);
 });
+app.use(cookieParser());
+app.use(express.json());
+
+// REDIS
+const Redis = require("ioredis");
+const RedisStore = require("connect-redis").default;
+const redisClient = new Redis();
 
 // CORS policy
 const cors = require("cors");
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:6379"], // .env ??
-    credentials: true, // .env ??
-    methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"], // .env ??
-  })
-);
-app.use(cookieParser());
-app.use(express.json());
-// Sessions
-app.use(
-  session({
-    secret: "your-secret-key", // put in .ENV
-    resave: false,
-    saveUninitialized: false,
-    // store: new RedisStore({client: redisClient}), // this means where to store sessions. store on redis.
-    cookie: {
-      maxAge: 60 * 60 * 1000, // 1 hour in milliseconds / hopefully able to change because of remember me.
-    },
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
   })
 );
 
 // MySQL connection
 const mysql = require("mysql2");
 const connection = mysql.createConnection({
-  host: "localhost", // .env ??
-  user: "root", // .env ??
-  password: "faris123", // .env ??
-  database: "librarydb", // .env ??
+  host: process.env.SQLHOST,
+  user: process.env.SQLUSER,
+  password: process.env.SQLPASSWORD,
+  database: process.env.SQLDATABASE,
 });
+
+// Sessions
+app.use(
+  session({
+    store: new RedisStore({client: redisClient}),
+    secret: process.env.SESSIONSECRET,
+    resave: false,
+    saveUninitialized: false,
+    name: "sid",
+    cookie: {
+      secure: false,
+      httpOnly: false,
+      sameSite: false,
+      httpOnly: true,
+      secure: false,
+      maxAge: 60 * 60 * 1000 * 3, // 3 hour in milliseconds
+    },
+  })
+);
 
 // Routers & Routes
 const authRoute = require("./routes/auth.js")(connection, mysql);

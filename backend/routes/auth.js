8c76a885ec21a460a6f790417990d59f4a00fbcc
FarisMuhovic <faris.fare123456@gmail.com>
2023-10-13 20:04:18 +0200

    backend session checker route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -11,23 +11,36 @@ module.exports = (connection, mysql) => {
       function (err, results, fields) {
         if (results.length > 0) {
           if (comparePasswords(password, results[0].password)) {
-            console.log("logged in");
             req.session.user = {
               email: email,
               id: results[0].id,
               // here goes other info
             };
-            console.log(req.session);
             res.status(200).json({message: "User logged in"});
           } else {
-            res.status(400).json({message: "Wrong password"});
+            res.status(400).json({message: "Wrong info"});
           }
         } else {
-          console.log("invalid info");
+          res.status(400).json({message: "Wrong info"});
         }
       }
     );
   });
-
+  router.get("/loggedin", (req, res) => {
+    if (req.session.user) {
+      const {email, id} = req.session.user;
+      connection.query(
+        "select email,id from staff where email =" +
+          mysql.escape(email) +
+          "and id =" +
+          mysql.escape(id),
+        function (err, results, field) {
+          res.status(200).json({message: "user is logged in"});
+        }
+      );
+    } else {
+      res.status(401).json({message: "invalid session"});
+    }
+  });
   return router;
 };
